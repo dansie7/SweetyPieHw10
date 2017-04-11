ebf8c458cbdfbf6bb454c41502feb3342e2938de
Daniel Tayler <danieltayler@mail.weber.edu>
2017-04-11 16:18:51 +0000

    first bloody commit

---
+++ SweetyPieHw10Task3.c
@@ -0,0 +1,30 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  SweetyPieHw10Task3.c
+ *       	Usage:  ./SweetyPieHw10Task3.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/11/2017 04:18:16 PM
+ *
+ Compiler:  gcc -Wall -Werror -lm 
+ *         Author:  Daniel Tayler (), danieltayler@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int  main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
