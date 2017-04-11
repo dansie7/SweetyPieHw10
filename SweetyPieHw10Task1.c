082adaaf44479b592bb6a12e4601bca2952f5167
Sharon Dansie <sharondansie@mail.weber.edu>
2017-04-11 16:21:20 +0000

    intial commit for everything

---
+++ SweetyPieHw10Task1.c
@@ -0,0 +1,29 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  SweetyPieHw10Task1.c
+ *       	Usage:  ./SweetyPieHw10Task1.c
+ *    Description:  Task 1
+ *
+ *        Version:  1.0
+ *        Created:  04/11/2017 04:19:43 PM
+ *       Compiler:  gcc -Wall -Werror
+ *         Author:  Sharon Dansie (), sharondansie@weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+
