7f774fa4d2db3a003afe1ab1798b53f5912b1fcd
Daniel Tayler <danieltayler@mail.weber.edu>
2017-04-18 15:08:20 +0000

    confused

---
+++ mess.c
@@ -0,0 +1,36 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  mess.c
+ *       	Usage:  ./mess.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  04/14/2017 08:55:33 PM
+ *
+ Compiler:  gcc -Wall -Werror -lm 
+ *         Author:  Daniel Tayler (), danieltayler@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>		/* For Standard I/O */
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/* Main Program */
+int  main(int argc, char *argv[])
+{
+
+    FILE hello;
+    unsigned int gr;
+    gr = fgetc("mp3Header.txt");
+
+
+	return 0;
+}
+
+
+/* Function Defenitions */
+
+    
+
